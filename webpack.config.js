bdb7093025416b66369b2332341ccccdbbcb23ea
Its-Shashank <gaurshashank014@gmail.com>
2021-03-13 16:37:04 +0530

    eslint webpack prettier babel setup done

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
+    // eslint-disable-next-line no-undef
     path: path.join(__dirname, "/dist"),
     filename: "bundle.js",
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: ["babel-loader"],
       },
     ],
   },
@@ -22,5 +23,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
+    new ESLintPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 };
